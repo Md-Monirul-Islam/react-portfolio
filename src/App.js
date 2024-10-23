01742e5ec67f4239fd68c791dca4bb5dde03d983
munna <md.monirulmunna.cse@gmail.com>
2024-10-23 22:51:49 +0600

    successfully configure particles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
 import Home from "./containers/home/index";
 import About from "./containers/about";
 import Resume from "./containers/resume";
@@ -9,14 +11,90 @@ import Contact from "./containers/contact";
 import Navbar from "./components/navBar";
 
 function App() {
+  const particlesInit = async (main) => {
+    // Ensures the full tsparticles engine is loaded
+    await loadFull(main);
+  };
+
+  const particlesOptions = {
+    background: {
+      color: {
+        value: "#1d1d1d",
+      },
+    },
+    fpsLimit: 120,
+    interactivity: {
+      events: {
+        onClick: {
+          enable: true,
+          mode: "push",
+        },
+        onHover: {
+          enable: true,
+          mode: "repulse",
+        },
+        resize: true,
+      },
+      modes: {
+        push: {
+          quantity: 4,
+        },
+        repulse: {
+          distance: 200,
+          duration: 0.4,
+        },
+      },
+    },
+    particles: {
+      color: {
+        value: "#ffffff",
+      },
+      links: {
+        color: "#ffffff",
+        distance: 150,
+        enable: true,
+        opacity: 0.5,
+        width: 1,
+      },
+      move: {
+        direction: "none",
+        enable: true,
+        outModes: {
+          default: "bounce",
+        },
+        random: false,
+        speed: 6,
+        straight: false,
+      },
+      number: {
+        density: {
+          enable: true,
+          area: 800,
+        },
+        value: 80,
+      },
+      opacity: {
+        value: 0.5,
+      },
+      shape: {
+        type: "circle",
+      },
+      size: {
+        value: { min: 1, max: 5 },
+      },
+    },
+    detectRetina: true,
+  };
+
   return (
     <div className="App">
-      {/* particles js */}
+      {/* Particles js */}
+      <Particles id="particles" init={particlesInit} options={particlesOptions} />
 
-      {/* navbar */}
+      {/* Navbar */}
       <Navbar />
 
-      {/* main page content */}
+      {/* Main page content */}
       <Routes>
         <Route index path="/" element={<Home />} />
         <Route path="/about/" element={<About />} />
