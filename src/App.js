b0df0614669cb23889383d6b21b11f1bfc754022
munna <md.monirulmunna.cse@gmail.com>
2024-10-23 23:10:21 +0600

    create a another page for showing particles

--- src/App.js
+++ src/App.js
@@ -9,87 +9,17 @@ import Skills from "./containers/skills";
 import Portfolio from "./containers/portfolio";
 import Contact from "./containers/contact";
 import Navbar from "./components/navBar";
+import particles from "./utils/particles";
 
 function App() {
   const particlesInit = async (main) => {
-    // Ensures the full tsparticles engine is loaded
     await loadFull(main);
-  };
-
-  const particlesOptions = {
-    background: {
-      color: {
-        value: "#1d1d1d",
-      },
-    },
-    fpsLimit: 120,
-    interactivity: {
-      events: {
-        onClick: {
-          enable: true,
-          mode: "push",
-        },
-        onHover: {
-          enable: true,
-          mode: "repulse",
-        },
-        resize: true,
-      },
-      modes: {
-        push: {
-          quantity: 4,
-        },
-        repulse: {
-          distance: 200,
-          duration: 0.4,
-        },
-      },
-    },
-    particles: {
-      color: {
-        value: "#ffffff",
-      },
-      links: {
-        color: "#ffffff",
-        distance: 150,
-        enable: true,
-        opacity: 0.5,
-        width: 1,
-      },
-      move: {
-        direction: "none",
-        enable: true,
-        outModes: {
-          default: "bounce",
-        },
-        random: false,
-        speed: 6,
-        straight: false,
-      },
-      number: {
-        density: {
-          enable: true,
-          area: 800,
-        },
-        value: 80,
-      },
-      opacity: {
-        value: 0.5,
-      },
-      shape: {
-        type: "circle",
-      },
-      size: {
-        value: { min: 1, max: 5 },
-      },
-    },
-    detectRetina: true,
   };
 
   return (
     <div className="App">
       {/* Particles js */}
-      <Particles id="particles" init={particlesInit} options={particlesOptions} />
+      <Particles id="particles" init={particlesInit} options={particles} />
 
       {/* Navbar */}
       <Navbar />
