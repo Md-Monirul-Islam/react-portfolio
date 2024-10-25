6e2567cd3e5431a6aa96d9b76cd9a5df525af175
munna <md.monirulmunna.cse@gmail.com>
2024-10-25 22:45:15 +0600

    complete resume section

--- src/containers/resume/index.jsx
+++ src/containers/resume/index.jsx
@@ -2,7 +2,10 @@ import React from "react";
 import { BsInfoCircleFill } from "react-icons/bs";
 import PageHeaderContent from "../../components/pageHeaderContent";
 import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component";
+import 'react-vertical-timeline-component/style.min.css';
+import { MdWork } from "react-icons/md";
 import { data } from "./utils";
+import './styles.scss';
 
 
 const Resume = () => {
@@ -24,12 +27,28 @@ const Resume = () => {
                                 <VerticalTimelineElement
                                     key={index}
                                     className="timeline__experience__verticle-timeline-element"
+                                    contentStyle={{
+                                        background: 'none',
+                                        color: 'var(--yellow-theme-sub-text-color)',
+                                        border: '1.5px solid var(--yellow-theme-main-color)'
+                                    }}
+                                    // date="March 2024 - Present"
+                                    icon={<MdWork />}
+                                    iconStyle={{
+                                        background: '#181818',
+                                        color: 'var(--yellow-theme-main-color)'
+                                    }}
                                 >
                                     <div className="verticle-timeline-element-title-wrapper">
-                                        <h3 className="verticle-timeline-element-title">
+                                        <h3>
                                             {item.title}
                                         </h3>
+                                        <h4>
+                                            --{item.company}
+                                        </h4>                                       
                                     </div>
+                                    <p className="verticle-timeline-element-title-wrapper-description">{item.description}</p>
+                                    <p className="verticle-timeline-element-title-wrapper-date">{item.date}</p>
                                 </VerticalTimelineElement>
                             ))
                         }
@@ -37,6 +56,39 @@ const Resume = () => {
                 </div>
                 <div className="timeline__education">
                     <h3 className="timeline__education__header-text">Education</h3>
+                        <VerticalTimeline 
+                            layout="1-column"
+                            lineColor="var(--yellow-theme-main-color)"
+                        >
+                        {
+                                data.education.map((item,index)=>(
+                                    <VerticalTimelineElement
+                                        key={index}
+                                        className="timeline__experience__verticle-timeline-element"
+                                        contentStyle={{
+                                        background: 'none',
+                                        color: 'var(--yellow-theme-sub-text-color)',
+                                        border: '1.5px solid var(--yellow-theme-main-color )'
+                                    }}
+                                    icon={<MdWork />}
+                                    iconStyle={{
+                                        background: '#181818',
+                                        color: 'var(--yellow-theme-main-color)'
+                                    }}
+                                    >
+                                        <div className="verticle-timeline-element-title-wrapper">
+                                            <h3>
+                                                {item.title}
+                                            </h3>
+                                            <h4>
+                                                --{item.company}
+                                            </h4>                                           
+                                        </div>
+                                        <p className="verticle-timeline-element-title-wrapper-date">{item.date}</p>
+                                    </VerticalTimelineElement>
+                                ))
+                            }
+                        </VerticalTimeline>
                 </div>
             </div>
         </section>
--- src/containers/resume/utils.js
+++ src/containers/resume/utils.js
@@ -1,19 +1,40 @@
 export const data = {
     expericence: [
         {
-            title: 'Junior Software Engineer',
+            title: 'Jr. Software Engineer',
             company: 'Romance Cable Network',
-            description: "Developed a REST API and MySQL to store data from Employee Reporting System. Developed a full-stack web application using Django, React.JS, and MySQL."
+            description: "Developed a REST API and MySQL to store data from Employee Reporting System. Developed a full-stack web application using Django, React.JS, and MySQL.",
+            date: 'March 2024 - Present'
         },
         {
             title: 'Project Assistant',
-            company: 'Prof.Dr. Sayed Md Galib (Chairman, dept. of CSE, JUST',
-            description: "Developed a REST API and MySQL to store data from Employee Reporting System. Developed a full-stack web application using Django, React.JS, and MySQL."
+            company: 'Prof. Dr. Sayed Md Galib (Chairman, dept. of CSE, JUST)',
+            description: "Developed a REST API and MySQL to store data from Employee Reporting System. Developed a full-stack web application using Django, React.JS, and MySQL.",
+            date: 'Dec. 2022 - Jan. 2024'
         },
         {
             title: 'Teaching Assistant',
             company: 'Shikkhar Alo Coaching Center',
-            description: "As a Math Teaching Assistant at Shikkhar Alo Coaching Center, my role is to enhance the learning experience for students by supporting the lead instructor in delivering effective math education. I provide one-on-one assistance, clarify challenging concepts, and ensure students feel confident in their math skills."
+            description: "As a Math Teaching Assistant at Shikkhar Alo Coaching Center, my role is to enhance the learning experience for students by supporting the lead instructor in delivering effective math education. I provide one-on-one assistance, clarify challenging concepts, and ensure students feel confident in their math skills.",
+            date: 'Jun. 2021 - Aug. 2022'
+        },
+    ],
+
+    education: [
+        {
+            title: 'Bachelor in Computer Science and Engineering',
+            company: 'Jashore University of Science and Technology',
+            date: "Jan. 2019 – Dec 2023"
+        },
+        {
+            title: 'HSC (Science)',
+            company: 'K.A.Z Scholl and College',
+            date: "April 2015 - April 2017"
+        },
+        {
+            title: 'SSC (Science)',
+            company: 'K.A.Z Scholl and College',
+            date: "Jan. 2013 - Feb. 2015"
         },
     ]
 }
