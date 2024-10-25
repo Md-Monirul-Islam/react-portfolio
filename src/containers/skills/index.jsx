66d090e8a274fa7355aa4c1fa5db2551af40c73e
munna <md.monirulmunna.cse@gmail.com>
2024-10-25 12:11:32 +0600

    complete design for skills page

--- src/containers/skills/index.jsx
+++ src/containers/skills/index.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { BsInfoCircleFill } from "react-icons/bs";
 import PageHeaderContent from "../../components/pageHeaderContent";
+import { skilsData } from "./utils";
+import { Animate, AnimateKeyframes } from "react-simple-animate";
+import { Line } from "rc-progress";
+import './styles.scss';
 
 
 const Skills = () => {
@@ -10,6 +14,50 @@ const Skills = () => {
                 headerText = 'My Skills'
                 icon = {<BsInfoCircleFill size={40} />}
             />
+            <div className="skills__content-wrapper">
+                {
+                    skilsData.map((item,index) => (
+                        <div key={index} className="skills__content-wrapper__inner-content">
+                            <Animate
+                                play
+                                duration={1}
+                                delay={0.3}
+                                start={{
+                                    transform:'translateX(-200px)'
+                                }}
+                                end={{
+                                    transform:'translateX(0px)'
+                                }}
+                            >
+                                <h3 className="skills__content-wrapper__inner-content__category-text">{item.label}</h3>
+                                <div className="skills__content-wrapper__inner-content__progressbar-container">
+                                    {
+                                        item.data.map((skillItem,index)=>(
+                                            <AnimateKeyframes
+                                                play
+                                                duration={1}
+                                                keyframes={['opacity : 1', 'opacity : 0']}
+                                                iterationCount={1}
+                                            >
+                                                <div className="progressbar-wrapper" key={index}>
+                                                    <p>{skillItem.skillName}</p>
+                                                    <Line
+                                                        percent={skillItem.percentage}
+                                                        strokeWidth={2}
+                                                        strokeColor={"var(--yellow-theme-main-color)"}
+                                                        trailWidth={2}
+                                                        strokeLinecap="square"
+                                                    ></Line>
+                                                </div>
+                                            </AnimateKeyframes>
+                                        ))
+                                    }
+                                </div>
+                            </Animate>
+                        </div>
+                    ))
+                }
+            </div>
         </section>
     )
 }
---
+++ src/containers/skills/utils.js
@@ -0,0 +1,65 @@
+export const skilsData = [
+    {
+        label: "FRONT END",
+        data: [
+            {
+                skillName: "HTML",
+                percentage: "90",
+            },
+            {
+                skillName: "CSS",
+                percentage: "80",
+            },
+            {
+                skillName: "BOOTSTRAP",
+                percentage: "90",
+            },
+            {
+                skillName: "JAVASCRIPT",
+                percentage: "80",
+            },
+            {
+                skillName: "REACT",
+                percentage: "80",
+            },
+            {
+                skillName: "VUE",
+                percentage: "60",
+            },
+        ]
+    },
+    {
+        label: "BACK END",
+        data: [
+            {
+                skillName: "PYTHON",
+                percentage: "80",
+            },
+            {
+                skillName: "DJANGO",
+                percentage: "80",
+            },
+        ]
+    },
+    {
+        label: "DATABASE",
+        data: [
+            {
+                skillName: "MYSQL",
+                percentage: "90",
+            },
+            {
+                skillName: "POSTGRESQL",
+                percentage: "80",
+            },
+            {
+                skillName: "FIREBASE",
+                percentage: "70",
+            },
+            {
+                skillName: "SQLite",
+                percentage: "90",
+            }
+        ]
+    }
+]
