b30931bcdf955420f52ad88147034dff05236f8c
munna <md.monirulmunna.cse@gmail.com>
2024-10-25 21:15:30 +0600

    start design to resume page

--- src/containers/resume/index.jsx
+++ src/containers/resume/index.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { BsInfoCircleFill } from "react-icons/bs";
 import PageHeaderContent from "../../components/pageHeaderContent";
+import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component";
+import { data } from "./utils";
 
 
 const Resume = () => {
@@ -10,6 +12,33 @@ const Resume = () => {
                 headerText = 'My Resume'
                 icon = {<BsInfoCircleFill size={40} />}
             />
+            <div className="timeline">
+                <div className="timeline__experience">
+                    <h3 className="timeline__experience__header-text">Experience</h3>
+                    <VerticalTimeline 
+                        layout="1-column"
+                        lineColor="var(--yellow-theme-main-color)"
+                    >
+                        {
+                            data.expericence.map((item,index)=>(
+                                <VerticalTimelineElement
+                                    key={index}
+                                    className="timeline__experience__verticle-timeline-element"
+                                >
+                                    <div className="verticle-timeline-element-title-wrapper">
+                                        <h3 className="verticle-timeline-element-title">
+                                            {item.title}
+                                        </h3>
+                                    </div>
+                                </VerticalTimelineElement>
+                            ))
+                        }
+                    </VerticalTimeline>
+                </div>
+                <div className="timeline__education">
+                    <h3 className="timeline__education__header-text">Education</h3>
+                </div>
+            </div>
         </section>
     )
 }
---
+++ src/containers/resume/utils.js
@@ -0,0 +1,19 @@
+export const data = {
+    expericence: [
+        {
+            title: 'Junior Software Engineer',
+            company: 'Romance Cable Network',
+            description: "Developed a REST API and MySQL to store data from Employee Reporting System. Developed a full-stack web application using Django, React.JS, and MySQL."
+        },
+        {
+            title: 'Project Assistant',
+            company: 'Prof.Dr. Sayed Md Galib (Chairman, dept. of CSE, JUST',
+            description: "Developed a REST API and MySQL to store data from Employee Reporting System. Developed a full-stack web application using Django, React.JS, and MySQL."
+        },
+        {
+            title: 'Teaching Assistant',
+            company: 'Shikkhar Alo Coaching Center',
+            description: "As a Math Teaching Assistant at Shikkhar Alo Coaching Center, my role is to enhance the learning experience for students by supporting the lead instructor in delivering effective math education. I provide one-on-one assistance, clarify challenging concepts, and ensure students feel confident in their math skills."
+        },
+    ]
+}
