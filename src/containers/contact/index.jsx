809706a082527b9e1753f203039730c466a0428b
unknown <md.monirulmunna.cse@gmail.com>
2024-10-27 16:18:14 +0600

    complete contact page and complete the portfolio

--- src/containers/contact/index.jsx
+++ src/containers/contact/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BsInfoCircleFill } from "react-icons/bs";
 import PageHeaderContent from "../../components/pageHeaderContent";
 import { Animate } from "react-simple-animate";
+import './styles.scss'
 
 const Contact = () => {
     return (
@@ -24,6 +25,35 @@ const Contact = () => {
                     }}
                 >
                    <h3 className="contact__content__header-text">Let's Talk</h3> 
+                </Animate>
+                <Animate
+                    play
+                    duration={1}
+                    delay={0}
+                    start={{
+                        transform: "translateX(200px)"
+                    }}
+                    end={{
+                        transform: "translateX(0px)"
+                    }}
+                >
+                    <div className="contact__content__form">
+                        <div className="contact__content__form__controlswrapper">
+                            <div>
+                                <input name="name" className="inputName" type="text" required />
+                                <label htmlFor="name" className="nameLabel">Name</label>
+                            </div>
+                            <div>
+                                <input name="email" className="inputEmail" type="email" required />
+                                <label htmlFor="email" className="emailLabel">Email</label>
+                            </div>
+                            <div>
+                                <textarea rows={5} name="description" className="inputDescription" type="text" required />
+                                <label htmlFor="description" className="DescriptionLabel">Description</label>
+                            </div>
+                        </div>
+                        <button>Submit</button>
+                    </div>
                 </Animate>
             </div>
         </section>
